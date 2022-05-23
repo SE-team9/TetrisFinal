b053f4909a523643e9e186ac4a688d238fae6409
leeeha <jxlhe46@seoultech.ac.kr>
2022-05-23 17:47:00 +0900

    게임 스레드 전체 코드에 한줄 삭제 아이템 적용

--- src/tetris/GameThread.java
+++ src/tetris/GameThread.java
@@ -101,11 +101,10 @@ public class GameThread extends Thread {
 	private void startDefaultMode() {
 		while(true) {
 			// nba 객체 처음 생성될 때 설정된 nextBlock 가져오기 
-			//ga.spawnBlock(nba.getNextBlock());
+			ga.spawnBlock(nba.getNextBlock());
 			
 			// 다음 블럭 업데이트 
 			nba.updateNextBlock();
-			//ga.setBlockIndex(nba.getBlockIndex());
 			
 			// 블럭이 위쪽 경계를 넘지 않으면 계속 낙하 
 			while (ga.moveBlockDown()) {
@@ -199,11 +198,10 @@ public class GameThread extends Thread {
 	private void startDefaultMode_pvp() {
 		while (true) {
 			// --------------------------------------------------------- 새로운 블럭 생성
-			// nba 객체 처음 생성될 때 설정된 nextBlock 가져오기 
-			//ga.spawnBlock(nba.getNextBlock());
 			
-			// 다음 블럭 업데이트 
-			nba.updateNextBlock();
+			ga.spawnBlock(nba.getNextBlock());
+			nba.updateNextBlock(); 
+			
 			// --------------------------------------------------------- 한칸씩 블럭 내리기
 			while (ga.moveBlockDown()) {
 				score++;
@@ -235,6 +233,7 @@ public class GameThread extends Thread {
 			}
       
 			// --------------------------------------------------------- 배경으로 블럭 이동 / 완성된 줄 삭제 / 공격 줄 가져오기
+			
 			ga.saveBackground();
 			ga.moveBlockToBackground();
 			int curCL = ga.clearLines_pvp();
@@ -273,6 +272,8 @@ public class GameThread extends Thread {
 	// 아이템 모드 
 	private void startItemMode() {
 		while (true) {
+			// ------------------------------------------------------- 새 블럭 생성, 다음 블럭 갱신 
+			
 			// 블럭 모양은 참조할 수 있지만, L문자를 붙이는 건 따로 조절해야 한다. 
 			ga.spawnBlock(nba.getNextBlock());
 			
@@ -281,9 +282,7 @@ public class GameThread extends Thread {
 				nba.setIsItem(true);
 				nba.updateNextItem(); // 아이템으로 (블럭 인덱스 업데이트)
 				
-				// 여기서 get, set을 이용할 수밖에 없는 이유는
-				// GameForm에서 nba가 ga보다 먼저 생성되기 때문에, nba의 생성자에 ga를 넘겨줄 수 없기 때문이다.
-				// 그렇다고 원래대로 ga를 먼저 생성하면 지금 로직이 틀어진다. nba에서 생성된 블럭을 ga에서 가져오고 있으니깐. 
+				// nba와 ga의 블럭에서 L문자가 붙는 위치가 동일하도록  
 				if(nba.getBlockIndex() == 11) {
 					ga.setRandomIndex(nba.getRandIndex());
 				}
@@ -292,7 +291,8 @@ public class GameThread extends Thread {
 				nba.updateNextBlock(); // 일반 블럭으로
 			}
 		
-			//  현재 블럭이 바닥이나 다른 블럭에 닿으면 false
+			// ------------------------------------------------------- 한칸씩 내리기 
+			
 			while (ga.moveBlockDown()) { 
 				score++; // 한 단위씩 계속 증가 
 				gf.updateScore(score);
@@ -321,7 +321,7 @@ public class GameThread extends Thread {
 				return; // 게임 스레드 종료 
 			}
 			
-			// ------------------------------------------ 현재 블럭이 바닥이나 다른 블럭에 닿은 경우 
+			// ------------------------------------------------------- 현재 블럭이 바닥이나 다른 블럭에 닿은 경우 
 			
 			// 현재 블럭이 아이템이면, 그에 따른 기능 수행 
 			if(curIsItem) {
@@ -340,6 +340,8 @@ public class GameThread extends Thread {
 
 				ga.setIsItem(false);
 				curIsItem = false; // 플래그 업데이트
+				
+				continue; // 그 외의 아이템에 대해서는 점수, 레벨, 속도 변화 없음.
 			}
 			else {
 				// 현재 블럭이 기본 블럭이면, 배경으로 전환 
@@ -383,17 +385,26 @@ public class GameThread extends Thread {
 	// 대전+아이템
 	private void startItemMode_pvp() {
 		while (true) {
-			// --------------------------------------------------------- 새로운 블럭생성
-			//ga.spawnBlock(nba.getNextBlock());
-
+			// --------------------------------------------------------- 새 블럭 생성, 다음 블럭 갱신 
+			
+			// 블럭 모양은 참조할 수 있지만, L문자를 붙이는 건 따로 조절해야 한다. 
+			ga.spawnBlock(nba.getNextBlock());
+			
+			// 다음 블럭 표시 
 			if (nextIsItem) {
-				nba.updateNextItem();
 				nba.setIsItem(true);
-			} else {
-				nba.updateNextBlock();
+				nba.updateNextItem(); // 아이템으로 (블럭 인덱스 업데이트)
+				
+				// nba와 ga의 블럭에서 L문자가 붙는 위치가 동일하도록  
+				if(nba.getBlockIndex() == 11) {
+					ga.setRandomIndex(nba.getRandIndex());
+				}
+				
+			}else {
+				nba.updateNextBlock(); // 일반 블럭으로
 			}
 
-			// --------------------------------------------------------- 한 칸씩 블럭 내리기
+			// --------------------------------------------------------- 한칸씩 내리기
 			while (ga.moveBlockDown()) {
 				score++;
 				gf.updateScore(score, userID);
@@ -421,66 +432,80 @@ public class GameThread extends Thread {
 				break;
 			}
 	
+			// ---------------------------------------------------------- 현재 블럭이 바닥이나 다른 블럭에 닿은 경우 
+			
+			// ------------------------------------------ 현재 블럭이 아이템인 경우 
 			if (curIsItem) {
-				// ------------------------------------------ 현재 블럭이 아이템인 경우 
 				ga.twinkleItem();
 				ga.execItemFunction();
 
-				// 기본 블럭으로 초기화 
-				ga.setIsItem(false);
-				curIsItem = false;
-			} else { 
-				// ------------------------------------------ 현재 블럭이 기본 블럭인 경우 
-				ga.saveBackground(); // 싱글 모드 코드에서 부분만 추가됨. 
-				ga.moveBlockToBackground();
+				// 한줄 삭제 아이템에 대한 점수 부여 
+				if(ga.getOLCflag()) {
+					score += 1;
+					gf.updateScore(score);
+					System.out.println("score after OLC item: " + score);
+					totalClearedLine += 1;
+					
+					ga.setOLCflag(false);
+				}
 
-				// 3줄 이상 삭제해서 nextIsItem이 true가 된 경우  
-				if (nextIsItem) {
+				ga.setIsItem(false);
+				curIsItem = false; // 플래그 업데이트
+				
+				continue; // 아이템에 대해서는 점수, 레벨, 속도, 공격 줄 변화 없음.
+			} 
+			// ------------------------------------------ 현재 블럭이 기본 블럭인 경우 
+			else { 
+				ga.moveBlockToBackground();
+				
+				// 현재 블럭이 배경으로 전환된 후에 다음 블럭이 아이템이면 
+				if(nextIsItem) {
+					// 다음 블럭은 다시 기본 블럭으로 표시! 
 					nextIsItem = false;
 					nba.setIsItem(false);
 					
-					curIsItem = true; 
-					ga.setIsItem(true); // 이제 아이템 등장 
+					// 이제 아이템 블럭 등장! 
+					curIsItem = true;
+					ga.setIsItem(true);
+					
+					// 여기서 한줄 삭제 아이템의 인덱스를 전달해줘야 L문자를 그릴 수 있음.
+					ga.setBlockIndex(nba.getBlockIndex());
 				}
-			}
-
-			// --------------------------------------------------------- 완성된 줄 삭제, 공격 줄 가져오기
-			int curCL = ga.clearLines_pvp();
-			gf.getAttackLines(userID);
-
-			// --------------------------------------------------------- 줄 삭제에 따른 점수, 레벨, 낙하 속도
-			
-			if (curCL == 1) { // 한 줄 삭제
-				score += curCL;
-				gf.updateScore(score);
-				totalClearedLine += curCL;
-
-			} else if (curCL >= 2) { // 두 줄 이상 삭제
-				score += (10 * curCL); // 보너스 점수
-				gf.updateScore(score);
-				totalClearedLine += curCL;
-			}
-			
-			int lvl = totalClearedLine / linePerLevel + 1;
-			if (lvl > level) {
-				// 레벨 갱신
-				level = lvl;
-				gf.updateScore(level, userID);
 				
-				// 레벨에 따른 속도 상승 
-				if (interval > 300) {
-					interval -= speedupPerLevel;
+				// --------------------------------------------------------- 완성된 줄 삭제, 공격 줄 가져오기
+				int curCL = ga.clearLines_pvp();
+				gf.getAttackLines(userID);
 
-					score += (10 * level); // 보너스 점수
+				// --------------------------------------------------------- 줄 삭제에 따른 점수, 레벨, 낙하 속도
+				
+				if (curCL == 1) { // 한 줄 삭제
+					score += curCL;
 					gf.updateScore(score);
+					totalClearedLine += curCL;
+
+				} else if (curCL >= 2) { // 두 줄 이상 삭제
+					score += (10 * curCL); // 보너스 점수
+					gf.updateScore(score);
+					totalClearedLine += curCL;
 				}
-			}
-			
-			// linePerItem 이상 줄 삭제하면, 다음 블럭을 아이템으로 설정하기
-			int temp = totalClearedLine / linePerItem;
-			if(temp > itemCount) {
-				itemCount = temp; // 아이템이 등장한 횟수 갱신 
-				nextIsItem = true; // 다음 블럭을 아이템으로 설정 
+				
+				int lvl = totalClearedLine / linePerLevel + 1;
+				if (lvl > level) {
+					// 레벨 갱신
+					level = lvl;
+					gf.updateScore(level, userID);
+					
+					// 레벨에 따른 속도 상승 
+					if (interval > 300) {
+						interval -= speedupPerLevel;
+
+						score += (10 * level); // 보너스 점수
+						gf.updateScore(score);
+					}
+				}
+				
+				// 줄 삭제에 따른 아이템 블럭 설정 
+				updateItemState();
 			}
 		}
 	}
@@ -491,7 +516,7 @@ public class GameThread extends Thread {
 		
 		while (true) {
 			// --------------------------------------------------------- 새로운 블럭 생성
-			//ga.spawnBlock(nba.getNextBlock());
+			ga.spawnBlock(nba.getNextBlock());
 			nba.updateNextBlock();
 
 			// --------------------------------------------------------- 한칸씩 블럭 내리기
@@ -518,6 +543,8 @@ public class GameThread extends Thread {
 			}
 
 			// --------------------------------------------------------- 게임 종료 확인
+			
+			// TODO: 조건문에 의하면 userID가 1일 때만 종료될텐데??? 
 			if (ga.isBlockOutOfBounds() || (time <= 0 && userID == 1)) {
 				gf.interrupt_Opp(userID);
 				JOptionPane.showMessageDialog(null, (3 - userID) + " Player Win!");
